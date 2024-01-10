96e1c31b9078c80f54be438f5d140466954e4adc
José Luis Cotán <joseluis.cotan@iesdonana.org>
2024-01-10 21:18:02 +0100

    8

--- A08-U05.js
+++ A08-U05.js
@@ -1,9 +1,27 @@
+let asig = ["Empresa", "Despliegue", "Diseño", "Cliente", "Servidor"];
+let prof = ["Juana", "Miguel Ángel", "Nacho", "Rafa", "Ricardo"]
+
 let rellenar = document.getElementById("rellenar");
+let enviar = document.getElementById("enviar");
+let dias = document.getElementById("dias");
 
 rellenar.addEventListener('click', function () {
-    location.reload();
+    window.reload();
 });
 
 document.addEventListener('change', function (e) {
     document.body.style.backgroundColor = e.target.id;
 });
+
+enviar.addEventListener('click', function() {
+    let num = document.querySelector('select[id="asig"]').value;
+    // let dia = dias.options[dias.selecteIndex].text;
+    let marcados = document.querySelectorAll('.dias:checked');
+            let diasSeleccionados = [];
+            marcados.forEach(function(checkbox) {
+                diasSeleccionados.push(checkbox.name);
+            });
+    alert(`Se enviará a ${prof[num]} de la materia ${asig[num]} el ${
+        (diasSeleccionados[diasSeleccionados.length - 1])
+    }.`);
+})
